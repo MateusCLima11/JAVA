c405de95aef86f8edf0d6870221f00a85547ae87
Mateus <mateuslimasesi@gmail.com>
2026-08-20 15:08:35 -0300

    Add files via upload

---
+++ Dw3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Dw3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o número: ");
+        int limite = sc.nextInt();
+        int numero = 1;
+        do {
+            System.out.println(numero);
+            numero++;
+        } while (numero <= limite);
+        sc.close();
+    }
+}
---
+++ While5.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class While5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int op = 0;
+        while (op != 5) {
+            System.out.println("Menu de opções:");
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Listar");
+            System.out.println("3 - Alterar");
+            System.out.println("4 - Excluir");
+            System.out.println("5 - Sair");
+            System.out.println("Escolha uma opção: ");
+            op = sc.nextInt();
+
+            switch (op) {
+                case 1:
+                    System.out.println("Cadastrar");
+                    break;
+                case 2:
+                    System.out.println("Listar");
+                    break;
+                case 3:
+                    System.out.println("Alterar");
+                    break;
+                case 4:
+                    System.out.println("Excluir");
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+        sc.close();
+    }
+}
